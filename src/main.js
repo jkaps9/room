f95749f9be691c6b94ada0b79ce7ca633476f8ab
Josh <15219491+jkaps9@users.noreply.github.com>
2025-10-16 17:16:23 -0400

    make slides change with buttons

--- src/main.js
+++ src/main.js
@@ -7,3 +7,54 @@ if (navToggle && mainNav) {
     mainNav.classList.toggle("visible");
   });
 }
+
+// Slides
+let currentSlideId = 1;
+const maxSlideId = 3;
+const slideComponents = document.querySelectorAll("[data-slideId]");
+const previousSlideButton = document.querySelector(".previous");
+const nextSlideButton = document.querySelector(".next");
+
+function hideAllComponents() {
+  slideComponents.forEach((slideComponent) => {
+    slideComponent.classList.add("hidden");
+  });
+}
+
+function showCurrentComponents() {
+  slideComponents.forEach((slideComponent) => {
+    if (
+      Number(slideComponent.getAttribute("data-slideId")) === currentSlideId
+    ) {
+      slideComponent.classList.remove("hidden");
+    }
+  });
+}
+
+function handlePreviousClick() {
+  if (currentSlideId === 1) {
+    currentSlideId = maxSlideId;
+  } else {
+    currentSlideId--;
+  }
+}
+
+function handleNextClick() {
+  if (currentSlideId === maxSlideId) {
+    currentSlideId = 1;
+  } else {
+    currentSlideId++;
+  }
+}
+
+previousSlideButton.addEventListener("click", () => {
+  handlePreviousClick();
+  hideAllComponents();
+  showCurrentComponents();
+});
+
+nextSlideButton.addEventListener("click", () => {
+  handleNextClick();
+  hideAllComponents();
+  showCurrentComponents();
+});
